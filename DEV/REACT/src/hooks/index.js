8648574d88b5ce80a2af19ceaf0eafd10affc94c
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-09-22 23:35:57 +0700

    use Theme

--- DEV/REACT/src/hooks/index.js
+++ DEV/REACT/src/hooks/index.js
@@ -1,5 +1,6 @@
 import useAnimation from './useInput';
+import useTheme from './useTheme';
 
-export { useAnimation };
+export { useAnimation, useTheme };
 
 //git : https://codesandbox.io/s/y7kr0vll4v
