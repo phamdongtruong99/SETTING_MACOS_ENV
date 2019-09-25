ad486ac79198a644351f2fa017f9c65a858ca16b
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-09-25 19:35:59 +0700

    Update useMedia

---
+++ DEV/REACT/src/hooks/useMedia.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useMedia = query => {
+  let [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    let media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    let listener = () => setMatches(media.matches);
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
+
+export default useMedia;
+
+// git: https://stackblitz.com/edit/react-5cmtay?file=useMedia.js
