1c77bc506f84c8acd196cc9e5aee6e34dff7c45f
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-09-27 23:22:40 +0700

    Update

--- DEV/REACT/src/hooks/useQueryParams.js
+++ DEV/REACT/src/hooks/useQueryParams.js
@@ -1,12 +1,7 @@
-import { useMemo } from 'react';
 import { useLocation } from 'react-router';
-import qs from 'query-string';
 
-const useQueryParams = () => {
-  const { search } = useLocation();
-  return useMemo(() => (search ? qs.parse(search.substr(1)) : {}), [search]);
-};
+const useSearchParams = () => new URLSearchParams(useLocation().search); // C1
 
-return useQueryParams;
+return useSearchParams;
 
 // git: https://codesandbox.io/s/lingering-currying-u0m52
