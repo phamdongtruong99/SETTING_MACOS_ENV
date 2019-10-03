751eba135f390337d578232475ff6907d8deff9f
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-10-03 08:45:42 +0700

    Update some hooks

---
+++ STATER/REACT/src/hooks/usePrevProp.js
@@ -0,0 +1,15 @@
+import { useRef, useEffect } from 'react';
+
+const usePrevProp = prop => {
+  const prevProp = useRef(undefined);
+
+  useEffect(() => {
+    prevProp.current = prop;
+  }, [prop]);
+
+  return prevProp.current;
+};
+
+export default usePrevProp;
+
+//git: https://github.com/donavon/use-prev-prop
