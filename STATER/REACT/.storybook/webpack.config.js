aaf0eeeab6eac02b40396c56f4705cb27d73b5c4
tranthaison1231 <thanhhuyenpoo13@gmail.com>
2019-12-10 09:21:14 +0700

    Update generic function for REACT_TS

---
+++ STATER/REACT/.storybook/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+const themeVars = lessToJs(
+  fs.readFileSync(path.join(__dirname, '../src/antTheme.less'), 'utf8'),
+);
+
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+    test: /\.(js|jsx)$/,
+    options: {
+      presets: ['@babel/react'],
+      plugins: [
+        [
+          'import',
+          {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+          },
+        ],
+      ],
+    },
+  });
+
+  config.module.rules.push({
+    test: /\.less$/,
+    loaders: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'less-loader',
+        options: {
+          modifyVars: themeVars,
+          javascriptEnabled: true,
+        },
+      },
+    ],
+    include: [
+      path.resolve(__dirname, '../src'),
+      /[\\/]node_modules[\\/].*antd/,
+    ],
+  });
+  return config;
+};
---
+++ STATER/REACT_TS/src/utils/array.ts
@@ -0,0 +1,7 @@
+const last = <T>(arr: Array<T>): T => {
+  return arr[arr.length - 1];
+};
+
+const makeArr = <T>(x: T): [T] => {
+  return [x];
+};
