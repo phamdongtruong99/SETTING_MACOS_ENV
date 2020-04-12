55f40a5cd6c723ca45886c3e8f19446c70ceaf89
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-04-12 11:01:03 +0700

    Update useClock.js

--- STATER/REACT/src/hooks/useClock.js
+++ STATER/REACT/src/hooks/useClock.js
@@ -1,1 +1,34 @@
+import { useState, useEffect } from "react";
 
+const formatDate = date => {
+  if (!date) return "";
+
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+  const seconds = `0${date.getSeconds()}`.slice(-2);
+
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+const useClock = () => {
+  const [timeString, setTimeString] = useState("");
+
+  useEffect(() => {
+    const clockInterval = setInterval(() => {
+      const now = new Date();
+
+      const newTimeString = formatDate(now);
+
+      setTimeString(newTimeString);
+    }, 1000);
+
+    return () => {
+      clearInterval(clockInterval);
+    };
+  }, []);
+
+  return { timeString };
+};
+
+export default useClock;
+// ex: https://codesandbox.io/s/useclock-9rmxf?file=/src/useClock.js:0-702
