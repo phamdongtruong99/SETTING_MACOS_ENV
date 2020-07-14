af124cb79f576b6d13702d13f5395b0796479c07
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-07-14 18:16:23 +0700

    Update file.js

--- STATER/REACT/src/utils/file.js
+++ STATER/REACT/src/utils/file.js
@@ -1,3 +1,4 @@
+'use strict';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import * as FileSaver from 'file-saver';
@@ -25,3 +26,22 @@ export const exportDataToExcel = (content, fileName) => {
   const data = new Blob([excelBuffer], { type: fileType });
   FileSaver.saveAs(data, fileName + fileExtension);
 };
+
+
+/**
+ *
+ * @param dataUrl
+ * @param fileName
+ * @returns {File}
+ */
+export const dataURLtoFile = (dataUrl, fileName) => {
+    const arr = dataUrl.split(',');
+    let mime = arr[0].match(/:(.*?);/)[1];
+    let bstr = atob(arr[1]);
+    let n = bstr.length;
+    let u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, { type: mime });
+}
