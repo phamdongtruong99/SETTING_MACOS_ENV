e07e826e987d64aef56f842030fe40b406349c4c
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-08-07 14:17:14 +0700

    Create 1590147973261-create_users.ts

---
+++ STATER/DENO/migrations/1590147973261-create_users.ts
@@ -0,0 +1,19 @@
+  
+import { Migration, Schema, dbDialects } from "../deps.ts";
+
+const dialect: dbDialects = "pg";
+
+export const up: Migration = () => {
+  let query = new Schema(dialect).create("users", (table) => {
+    table.uuid("id").primary();
+    table.text("name").notNullable();
+    table.text("email").notNullable().unique();
+    table.text("password").notNullable();
+  });
+
+  return query;
+};
+
+export const down: Migration = () => {
+  return new Schema(dialect).drop("users");
+};
