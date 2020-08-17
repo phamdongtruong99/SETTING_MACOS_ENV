344ccf38e7239e5a4116f49563a198bd39c355f1
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-08-17 20:01:43 +0700

    Create decorator.js

---
+++ STATER/NESTJS/src/utils/decorator.js
@@ -0,0 +1,13 @@
+exxport function cloneDecorators(from, to) {
+  Reflect.getMetadataKeys(from).forEach(key => {
+    const value = Reflect.getMetadata(key, from)
+    Reflect.defineMetadata(key, value, to)
+  })
+}
+
+exxport function clonePropDecorators(from, to, name) {
+  Reflect.getMetadataKeys(from, name).forEach(key => {
+    const value = Reflect.getMetadata(key, from, name)
+    Reflect.defineMetadata(key, value, to, name)
+  })
+}
