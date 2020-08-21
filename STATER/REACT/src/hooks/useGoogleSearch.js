d927f1621bdf395f98bfcd433d5e547c0faf5728
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-08-21 13:07:59 +0700

    Create useGoogleSearch.js

---
+++ STATER/REACT/src/hooks/useGoogleSearch.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+const useGoogleSearch = (term) => {
+  const [ data, setData ] = useState(null);
+  
+  useEffect(() => {
+    const fetchData = async() => {
+      fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx={CONTEXT_KEY}&q=${term}`
+     ).then( res => res.json())
+      .then(result => {
+        setData(result);
+      }
+  }, [term]);
+  
+  return { data }
+}
+
+export default useGoogleSearch
