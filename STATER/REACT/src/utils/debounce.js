4e7ade18b843b8e60a4eb1bdceac097d9fcb0eee
tranthaison1231 <43759215+tranthaison1231@users.noreply.github.com>
2020-09-02 13:18:20 +0700

    Update debounce.js

--- STATER/REACT/src/utils/debounce.js
+++ STATER/REACT/src/utils/debounce.js
@@ -1,11 +1,31 @@
-export function debounce(func, delay) {
-  return function (args) {
-    let previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall&&((this.lastCall - previousCall)<=delay)) 
-      {
-        clearTimeout(this.lastCallTimer);
-      }
-    this.lastCallTimer = setTimeout(() => func(args), delay);
-  }
+import restArguments from './restArguments.js';
+import delay from './delay.js';
+
+export default function debounce(func, wait, immediate) {
+  var timeout, result;
+
+  var later = function(context, args) {
+    timeout = null;
+    if (args) result = func.apply(context, args);
+  };
+
+  var debounced = restArguments(function(args) {
+    if (timeout) clearTimeout(timeout);
+    if (immediate) {
+      var callNow = !timeout;
+      timeout = setTimeout(later, wait);
+      if (callNow) result = func.apply(this, args);
+    } else {
+      timeout = delay(later, wait, this, args);
+    }
+
+    return result;
+  });
+
+  debounced.cancel = function() {
+    clearTimeout(timeout);
+    timeout = null;
+  };
+
+  return debounced;
 }
