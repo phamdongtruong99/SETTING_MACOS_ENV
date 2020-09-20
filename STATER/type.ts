22c0050374063dbc138bc4358aa2c47eab54656a
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-09-20 08:24:06 +0700

    Create type.ts

---
+++ STATER/type.ts
@@ -0,0 +1,14 @@
+export type PathOf<T extends object> = {
+  [K in keyof T]: T[K] extends object ? [K] | [K, ...PathOf<T[K]>]: [K]
+}[keyof T]
+
+export type TypeOf<T extends {[K: string]: any}, TPath extends SerialzedPathOf<T> = SerializedPathOf<T>> = 
+  TPath extends `${infer F}.${infer U}` ? TypeOf<T[F]> : TPath extends `${infer F}` ? T[F] : null;
+  
+export type SerializedPathOf<T extends object>= Join<Extract<PathOf<T>, string[]>, '.'>;
+
+export type Join<T extends string[], D extends string> =
+  T extends [] ? '' :
+  T extends [infer U] ? U:
+  T extends [unknown, ...infer U] ? `${T[0]}${D}${Join<U, D>}` :
+  string;
