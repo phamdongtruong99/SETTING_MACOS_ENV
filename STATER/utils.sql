fe3541236d3ced77daf6e2c19029405711f7e7b3
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-10-24 10:04:26 +0700

    Update utils.sql

--- STATER/utils.sql
+++ STATER/utils.sql
@@ -2,7 +2,13 @@ CREATE FUNCTION translate_jp(value TEXT)
     RETURNS TEXT AS
   $BODY$
     DECLARE
+      index INT;
+      full_width_array varchar[] = ARRAY['ガ', 'ギ', 'グ', 'ゲ', 'ゴ', 'ザ', 'ジ', 'ズ', 'ゼ', 'ゾ', 'ダ', 'ヂ', 'ヅ', 'デ', 'ド', 'バ', 'ビ', 'ブ', 'ベ', 'ボ', 'パ', 'ピ', 'プ', 'ペ', 'ポ', 'ヴ'];
+      half_width_array varchar[] = ARRAY['ｶﾞ', 'ｷﾞ', 'ｸﾞ', 'ｹﾞ', 'ｺﾞ', 'ｻﾞ', 'ｼﾞ', 'ｽﾞ', 'ｾﾞ', 'ｿﾞ', 'ﾀﾞ', 'ﾁﾞ', 'ﾂﾞ', 'ﾃﾞ', 'ﾄﾞ', 'ﾊﾞ', 'ﾋﾞ', 'ﾌﾞ', 'ﾍﾞ', 'ﾎﾞ', 'ﾊﾟ', 'ﾋﾟ', 'ﾌﾟ', 'ﾍﾟ', 'ﾎﾟ', 'ｳﾞ'];
     BEGIN
+      FOR index IN 1..26 LOOP
+        value = replace(value, full_width_array[index], half_width_array[index]);
+      END LOOP;
       value = TRANSLATE(
         value,
         'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンァィゥェォッャュョヮヰヱヵヶーガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペポヴあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんぁぃぅぇぉっゃゅょゎゐゑヵヶーがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゔ',
