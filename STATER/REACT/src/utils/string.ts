f074dd4b01828da97b8c244f554a40acf104e975
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-04 20:49:47 +0700

    Update string.ts

--- STATER/REACT/src/utils/string.ts
+++ STATER/REACT/src/utils/string.ts
@@ -291,3 +291,16 @@ export const gutters = {
   xxl: STEP_GUTTER * 8,
   xxxl: STEP_GUTTER * 9,
 };
+
+export function slugify(text: string): string {
+  return text
+    .toString() // Cast to string
+    .toLowerCase() // Convert the string to lowercase letters
+    .normalize("NFD") // The normalize() method returns the Unicode Normalization Form of a given string.
+    .trim() // Remove whitespace from both sides of a string
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-"); // Replace multiple - with single -
+}
+
+// slugify("Chao ban") => chao-ban
