d5ebf9b829fb82aae5d69134474e323ba3b6053e
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-05 12:23:12 +0700

    Update generic.ts

--- STATER/REACT/src/types/generic.ts
+++ STATER/REACT/src/types/generic.ts
@@ -7,3 +7,8 @@ type GenericFunctionConstructor<T> = (...args: any[]) => T;
 export type GenericFunction = GenericFunctionConstructor<any>;
 
 export type GenericVoidFunction = GenericFunctionConstructor<void | Promise<void>>;
+
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
