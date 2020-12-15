e73afbfc5d6b9ad394ba9178b1793ac13f231af4
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-15 11:14:50 +0700

    Update Flex.tsx

--- STATER/REACT/src/components/UIKit/Flex.tsx
+++ STATER/REACT/src/components/UIKit/Flex.tsx
@@ -2,22 +2,35 @@ import styled from 'styled-components';
 import * as CSS from 'csstype';
 
 interface Props {
-  direction: CSS.Property.FlexDirection;
-  justify: CSS.Property.JustifyContent;
-  align: CSS.Property.AlignItems;
+  direction?: CSS.Property.FlexDirection;
+  justify?: CSS.Property.JustifyContent;
+  align?: CSS.Property.AlignItems;
+  bg?: string;
+  circle?: boolean;
+  width?: number;
+  height?: number;
 }
 
-const Flex = styled.div`
-  display: flex;
-  flex-direction: ${(props: Props) => props.direction};
-  justify-content: ${(props: Props) => props.align};
-  align-items: ${(props: Props) => props.align};
-`;
+const Flex = styled.div(
+  ({ direction, justify, align, bg, width, height, circle }: Props) => {
+    return `
+      display: flex;
+      flex-direction: ${direction};
+      justify-content: ${justify};
+      align-items: ${align};
+      background-color: ${bg};
+      width: ${width ? `${width}px` : 'auto'};
+      height: ${height ? `${height}px` : 'auto'};
+      border-radius: ${circle ? 50 : 100}%;
+  `;
+  },
+);
 
 Flex.defaultProps = {
   direction: 'row',
   align: 'flex-start',
   justify: 'flex-start',
+  circle: false,
 };
 
 export default Flex;
