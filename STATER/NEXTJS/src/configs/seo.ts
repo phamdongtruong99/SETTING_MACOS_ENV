3f09f032100f991900eb6495bd56fb70f57883ae
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-15 15:24:46 +0700

    Create seo.ts

---
+++ STATER/NEXTJS/src/configs/seo.ts
@@ -0,0 +1,25 @@
+const SEO = {
+  title: "Recipes | Explore the best recipes",
+  description: "Explore the best recipes.",
+  openGraph: {
+    url: "recipes-next-hasura.vercel.app",
+    title: "Recipes | Explore the best recipes",
+    description: "Explore the best recipes.",
+    images: [
+      {
+        url: "https://github.com/LauraBeatris/recipes-next-hasura/blob/master/.github/docs/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Recipes | Explore the best recipes",
+      },
+    ],
+    site_name: "Recipes",
+  },
+  twitter: {
+    handle: "@lauradotjs",
+    site: "@lauradotjs",
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
