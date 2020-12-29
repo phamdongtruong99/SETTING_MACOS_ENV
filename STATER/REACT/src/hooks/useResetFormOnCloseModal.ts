63050dbf5d67267a585ef21e7189660895ccc201
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-29 17:09:43 +0700

    Create useResetFormOnCloseModal.ts

---
+++ STATER/REACT/src/hooks/useResetFormOnCloseModal.ts
@@ -0,0 +1,17 @@
+import { useRef, useEffect} from 'react'
+
+const useResetFormOnCloseModal = ({ form, visible }) => {
+  const prevVisibleRef = useRef();
+  useEffect(() => {
+    prevVisibleRef.current = visible;
+  }, [visible]);
+  const prevVisible = prevVisibleRef.current;
+
+  useEffect(() => {
+    if (!visible && prevVisible) {
+      form.resetFields();
+    }
+  }, [visible]);
+};
+
+export default useResetFormOnCloseModal;
