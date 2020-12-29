b9822b314c04330d74a2f3b506b2234b346464b0
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2020-12-29 17:57:21 +0700

    Update firebase.ts

--- STATER/REACTNATIVE/src/utils/firebase.ts
+++ STATER/REACTNATIVE/src/utils/firebase.ts
@@ -1,11 +1,40 @@
-export const convertToFirebaseDatabasePathName = (text: string) => {
-    return text.replace(/\./g, "!").replace(/#/g, "@")
-        .replace(/\$/g, "%").replace(/\[/g, "&")
-        .replace(/\]/g, "*")
-}
+import AsyncStorage from '@react-native-community/async-storage';
+import firebase from '@react-native-firebase/app';
+import messaging from '@react-native-firebase/messaging';
 
-export const revertFirebaseDatabasePathName = (text: string) => {
+const FirebaseService {
+ getToken: async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    console.log('fcmToken', fcmToken);
+    if (fcmToken) {
+      return fcmToken;
+    }
+    return '';
+  },
+ setToken: async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    console.log('fcmToken', fcmToken);
+    if (fcmToken) {
+      await AsyncStorage.setItem('fcmToken', fcmToken);
+    }
+  },
+ requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED
+      || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      await this.setToken();
+    }
+  },
+ revertFirebaseDatabasePathName: (text: string) => {
     return text.replace(/\!/g, ".").replace(/\@/g, "#")
         .replace(/\%/g, "$").replace(/\&/g, "[")
         .replace(/\*/g, "]")
+ },
+ convertToFirebaseDatabasePathName: (text: string) => {
+    return text.replace(/\./g, "!").replace(/#/g, "@")
+        .replace(/\$/g, "%").replace(/\[/g, "&")
+        .replace(/\]/g, "*")
+ }
 }
+export default FirebaseService;
