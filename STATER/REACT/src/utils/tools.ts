1a4efa0a0bb4f116c56dbfa92b4c7e0be055b021
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-07 09:52:11 +0700

    Update tools.ts

--- STATER/REACT/src/utils/tools.ts
+++ STATER/REACT/src/utils/tools.ts
@@ -243,3 +243,22 @@ export const getImageSizeBasedOnDeviceRatio = (size: number): number => {
   const {devicePixelRatio} = window;
   return size * devicePixelRatio;
 };
+
+
+export function forceDownload(url: string, fileName: string) {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.responseType = 'blob'
+    xhr.onload = function() {
+      const urlCreator = window.URL || window.webkitURL
+      const imageUrl = urlCreator.createObjectURL(this.response)
+      const tag = document.createElement('a')
+      tag.href = imageUrl
+      tag.download = fileName
+      document.body.appendChild(tag)
+      tag.click()
+      document.body.removeChild(tag)
+    }
+    xhr.send()
+  }
+
