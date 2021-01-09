d2888c1c76a36aca4e7534b368cdff2314b7efbc
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-09 21:29:03 +0700

    Create config.tsx

---
+++ STATER/NEXTJS/src/configs/config.tsx
@@ -0,0 +1,13 @@
+export const formConfig = {
+  validateMessages: {
+    required: '${label} is required!',
+    pattern: {
+      mismatch: '${label} is not valid!',
+    },
+  },
+  requiredMark: false,
+};
+
+export const appConfig = {
+  title: 'Smartos Client',
+};
