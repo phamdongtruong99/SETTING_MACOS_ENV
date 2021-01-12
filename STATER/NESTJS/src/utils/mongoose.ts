c0a4c4c96f99de028fdded576a125d8a96a2a2b5
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-12 14:48:11 +0700

    Create mongoose.ts

---
+++ STATER/NESTJS/src/utils/mongoose.ts
@@ -0,0 +1,22 @@
+export function readableURL(schema, options) {
+  const path = 'permalink';
+  schema.path(path, {});
+
+  schema.pre('validate', function(next) {
+    const actualValue = this.get(path);
+
+    if (actualValue && !this.isNew && !this.isModified(path)) {
+      return next();
+    }
+
+    const permalink =
+      actualValue ||
+      String(this[options.source])
+        .replace(/ /g, '-')
+        .toLowerCase();
+
+    this.set(path, permalink);
+    next();
+  });
+}
+
