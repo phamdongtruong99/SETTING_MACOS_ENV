743c29b8924edfa944f95521162b2e51b92d485a
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-15 08:42:52 +0700

    Update .eslintrc.js

--- STATER/REACT/.eslintrc.js
+++ STATER/REACT/.eslintrc.js
@@ -41,8 +41,10 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.ts'] }],
     'react/jsx-props-no-spreading': 0,
+    'no-console': ['error'],
     'no-nested-ternary': ['error'],
-    'no-unneeded-ternary': ['error']
+    'no-unneeded-ternary': ['error'],
+    'no-else-return': ['error']
     'import/no-extraneous-dependencies': [
       error,
       {
