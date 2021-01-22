7cca58dafb9c75af4f073d577b43b89586f75b23
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-22 21:35:35 +0700

    Create useNetworkStatus.ts

---
+++ STATER/REACT/src/hooks/useNetworkStatus.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+export interface NetworkStatus {
+  isOnline: boolean
+  wasOffline: boolean
+}
+
+export const useNetworkStatus = (): NetworkStatus => {
+  const isOnline = navigator.onLine
+  const [networkStatus, setNetworkStatus] = useState({
+    isOnline,
+    wasOffline: !isOnline,
+  })
+  const handleOnline = () => {
+    setNetworkStatus((prevState) => ({ ...prevState, isOnline: true }))
+  }
+  const handleOffline = () => {
+    setNetworkStatus((prevState) => ({ ...prevState, isOnline: false, wasOffline: true }))
+  }
+  useEffect(() => {
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+
+    return () => {
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
+    }
+  }, [])
+
+  return networkStatus
+}
