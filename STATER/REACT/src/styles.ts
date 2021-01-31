15a9be425fd15c60db39556abb4d61f84618006a
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-01-31 12:20:24 +0700

    Update and rename styles.js to styles.ts

--- STATER/REACT/src/styles.ts
+++ STATER/REACT/src/styles.ts
@@ -79,10 +79,57 @@ const AppWrapper = createGlobalStyle`
   form .has-feedback .ant-input-affix-wrapper .ant-input-suffix {
     padding-right: 26px;
   }
+  .message-success {
+    .ant-message-custom-content {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: center;
+    }
+    .ant-message-notice-content {
+     background: #5FC17B;
+     box-shadow: 2px 1px 4px rgba(95, 193, 123, 0.45);
+     border-radius: 20px;
+     width: 405px;
+     color: #FFFFFF;
+     height: 40px;
+     font-weight: 550;
+     font-size: 16px;
+     line-height: 20px;
+     .anticon {
+       margin-left: 10px;
+       background: #5FC17B;
+       color: #fff;
+     }
+    }
+  }
+  .message-error {
+    .ant-message-custom-content {
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: center;
+    }
+    .ant-message-notice-content {
+     background: #E8505B;
+     box-shadow: 2px 1px 4px rgba(232, 80, 91, 0.45);
+     border-radius: 20px;
+     width: 200px;
+     color: #FFFFFF;
+     height: 40px;
+     font-weight: 550;
+     font-size: 16px;
+     line-height: 20px;
+     .anticon {
+       margin-left: 10px;
+       background: #E8505B;
+       color: #fff
+     }
+    }
+  }
   /*-----------------Add on-----------------------*/
   .t-500-14px-16px {
     font: normal 500 14px/16px ${({ theme }) => theme.fonts.primary};
   }
+
 `;
 
 export default AppWrapper;
