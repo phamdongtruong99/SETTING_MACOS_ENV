dc71568d870c28eac3509e6389f8bbcc6e72ded1
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-02-11 12:09:14 +0700

    Create base.entity.ts

---
+++ STATER/NESTJS/src/core/base.entity.ts
@@ -0,0 +1,16 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from "typeorm";
+
+export class BaseEntity {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @VersionColumn()
+  version: number;
+
+  @CreateDateColumn()
+  creationDate: Date;
+
+  @UpdateDateColumn()
+  updateDate: Date;
+}
