a46f1d1ff8da2331405f971d283ae9ae21145f6c
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-02-26 00:08:11 +0700

    Update url.ts

--- STATER/REACT/src/utils/url.ts
+++ STATER/REACT/src/utils/url.ts
@@ -131,3 +131,13 @@ export const shortenURLS = async (
   }
   return tinyURLS
 }
+
+export const getBrowserUrl = (): string => {
+	return window?.location?.href;
+};
+
+export const replaceBrowserUrl = (url: string) => {
+	if (window?.history?.replaceState) {
+		window.history.replaceState(null, '', url);
+	}
+};
