89f7a5fcf4d91da714c3b9a9f8e36b1a5530f4e2
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-03-07 09:57:04 +0700

    Create DatePicket.ts

---
+++ STATER/REACT/src/utils/DatePicket.ts
@@ -0,0 +1,9 @@
+
+import dayjsGenerateConfig from 'rc-picker/lib/generate/dayjs'
+import generatePicker from 'antd/lib/date-picker/generatePicker'
+
+const DatePicker = generatePicker(dayjsGenerateConfig)
+
+export default DatePicker
+
+//https://github.com/MauricioAntonioMartinez/mertion/blob/77311bafeed9125c9adee415f0931732629b4be9/frontend/src/util/DatePicket.ts
