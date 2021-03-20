ee67e7dcc39fb493183285cc28c0fbb3df2b00c4
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-03-20 15:47:55 +0700

    Update UserAvatar.tsx

--- STATER/REACT/src/components/UIKit/UserAvatar.tsx
+++ STATER/REACT/src/components/UIKit/UserAvatar.tsx
@@ -10,7 +10,7 @@ interface IUserAvatar {
 
 const UserAvatar: React.FC<IUserAvatar> = ({ url, name, style }) => {
   if(url){
-    return <Avatar style={style} src={user.avatarURL} />
+    return <Avatar style={style} src={url} />
   }
   return <Avatar style={style}>
         {name?.[0]?.toUpperCase() : ""}
