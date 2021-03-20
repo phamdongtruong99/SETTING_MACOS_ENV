3004373e2843971195be14ff73af8a9c51e04597
Shyn <43759215+tranthaison1231@users.noreply.github.com>
2021-03-20 10:45:50 +0700

    Update generic.ts

--- STATER/REACT/src/types/generic.ts
+++ STATER/REACT/src/types/generic.ts
@@ -12,3 +12,5 @@ export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] };  // https://viblo.asia/p/4-tinh-nang-rat-hay-tu-typescript-vyDZOMn95wj
+ 
